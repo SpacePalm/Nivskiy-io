7b961df1382a8e3f2c4bedfae06ac412f733e020
Гулимов Артём Анатольевич <gulimov.aa@greendatasoft.ru>
2023-03-27 00:31:16 +0300

    login/registration form connected to back-end

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import os
+import json
 import utils
 
 app = Flask(__name__)
@@ -29,78 +30,77 @@ def add_food():
 def add_train():
     return render_template("addzan.html")
 
-
-@app.route("/api/login_pass")
+@app.route("/api/login_pass", methods=['POST'])
 def login_pass():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.check_user(data["username"], data["password"])
 
 
-@app.route("/api/register")
+@app.route("/api/register", methods=['POST'])
 def register():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.register(data["username"], data["password"])
 
 
 @app.route("/api/change_password", methods=['POST'])
 def change_password():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.change_password(data["username"], data["password"])
 
 
 @app.route("/api/add_food", methods=['POST'])
 def api_add_food():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.add_food(data["id"], data["food"])
 
 
 @app.route("/api/add_train", methods=['POST'])
 def api_add_train():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.add_train(data["id"], data["train"])
 
 
 @app.route("/api/remove_train", methods=['POST'])
 def api_remove_train():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.remove_train(data["id"], data["train"])
 
 
 @app.route("/api/remove_food", methods=['POST'])
 def api_remove_food():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.remove_food(data["id"], data["food"])
 
 
 @app.route("/api/get_trains", methods=['POST'])
 def api_get_trains():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.get_trains(data["id"])
 
 
 @app.route("/api/get_food", methods=['POST'])
 def api_get_food():
-    if not request.json:
+    if not request.data:
         raise Exception
-    data = request.json
+    data = json.loads(request.data)
     return utils.get_food(data["id"])
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run("0.0.0.0", debug=True)
