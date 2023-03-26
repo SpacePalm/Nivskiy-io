6360eafa130d8087a411dbe76ceec2b40fb653ef
Гулимов Артём Анатольевич <gulimov.aa@greendatasoft.ru>
2023-03-26 21:30:25 +0300

    fixed add train buttons, add food buttons. display

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
-    $("#addzan-btn").click(function(){
-        window.location.href = "/addzan.html";
-    });
-    $("#addfood-btn").click(function(){
-        window.location.href = "/addfood.html";
-    });
-    $("#forgot").click(function(){
-        window.location.href = '/forgot';
-    })
+console.log($("#addzan-btn"))
+
+$("#sex-button").click(function () {
+    console.log("addzan")
+    window.location.href = "/add_train";
 });
+
+$("#addzan-btn").click(function () {
+    console.log("addzan")
+    window.location.href = "/add_train";
+});
+$("#addfood-btn").click(function () {
+    window.location.href = "/add_food";
+});
+$("#forgot").click(function () {
+    window.location.href = '/forgot';
+})
